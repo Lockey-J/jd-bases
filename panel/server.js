e044ec5559b1771083460cdceea13197282cdbd5
rylearn <zhijia21cn@163.com>
2021-06-28 17:45:26 +0800

    尝试修复

--- panel/server.js
+++ panel/server.js
@@ -131,7 +131,6 @@ async function step2() {
                 'Connection': 'Keep-Alive',
                 'Content-Type': 'application/x-www-form-urlencoded; Charset=UTF-8',
                 'Accept': 'application/json, text/plain, */*',
-                'Cookie': cookies,
                 'Referer': $.loginUrl,
                 'User-Agent': 'jdapp;android;10.0.5;11;0393465333165363-5333430323261366;network/wifi;model/M2102K1C;osVer/30;appBuild/88681;partner/lc001;eufv/1;jdSupportDarkMode/0;Mozilla/5.0 (Linux; Android 11; M2102K1C Build/RKQ1.201112.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/77.0.3865.120 MQQBrowser/6.2 TBS/045534 Mobile Safari/537.36',
                 'Host': 'plogin.m.jd.com',
